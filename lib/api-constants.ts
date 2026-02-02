a51d684b4543a540b7b5d24222c2b2d1479c17e7
TasnimurRahmanShakir <tasnim.bd.cse@gmail.com>
2026-02-02 15:35:04 +0600

    Add API base URL constants for `BASE_URL` and `BASE_URL2`.

--- lib/api-constants.ts
+++ lib/api-constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://localhost:7255/api";
-export const BASE_URL2 = "https://localhost:7255";
+export const BASE_URL = "https://api.kimzone.bd/api";
+export const BASE_URL2 = "https://api.kimzone.bd/";
